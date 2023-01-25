39151381100faf00d5b2e09af9003130132dc0c9
Kristian Freeman <kristian@kristianfreeman.com>
2023-01-25 08:11:02 -0600

    Consistent logger, lint

--- index.js
+++ index.js
@@ -1,72 +1,78 @@
 require('dotenv').config();
+
 const WebSocket = require('ws');
+const winston = require('winston');
 
-const error = error => console.log(JSON.stringify({ error }))
-const log = message => console.log(JSON.stringify({ message }))
-const rawLog = obj => console.log(JSON.stringify(obj))
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'home-assistant-event-listener' },
+  transports: [new winston.transports.Console()],
+});
 
-let hassUrl
+const EVENT_TYPE = process.env.EVENT_TYPE || '*';
 
+let hassUrl;
 if (!process.env.HOME_ASSISTANT_URL) {
-  log('Missing HOME_ASSISTANT_URL environment variable, defaulting to homeassistant.local:8123');
-  hassUrl = new URL(`ws://homeassistant.local:8123/api/websocket`)
+  logger.info('Missing HOME_ASSISTANT_URL environment variable, defaulting to homeassistant.local:8123');
+  hassUrl = new URL('ws://homeassistant.local:8123/api/websocket');
 } else {
-  hassUrl = new URL(`ws://${process.env.HOME_ASSISTANT_URL}/api/websocket`)
+  hassUrl = new URL(`ws://${process.env.HOME_ASSISTANT_URL}/api/websocket`);
 }
 
 if (!process.env.HOME_ASSISTANT_TOKEN) {
-  error("No HOME_ASSISTANT_TOKEN environment variable set")
-  process.exit(1)
+  logger.error('No HOME_ASSISTANT_TOKEN environment variable set');
+  process.exit(1);
 }
 
 const ws = new WebSocket(hassUrl);
 
-const handleMessage = message => {
+const handleMessage = (message) => {
   switch (message.type) {
     case 'auth_required':
-      log('Authenticating with Home Assistant')
+      logger.info('Authenticating with Home Assistant');
       ws.send(JSON.stringify({
         type: 'auth',
-        access_token: process.env.HOME_ASSISTANT_TOKEN
+        access_token: process.env.HOME_ASSISTANT_TOKEN,
       }));
-      break
+      break;
     case 'auth_ok':
-      log('Successfully authenticated with Home Assistant')
-      const event_type = process.env.EVENT_TYPE || '*'
-      log(`Subscribing to events of type ${event_type}`)
+      logger.info('Successfully authenticated with Home Assistant');
+      logger.info(`Subscribing to events of type ${EVENT_TYPE}`);
       ws.send(JSON.stringify({
         id: 1,
         type: 'subscribe_events',
-        event_type
+        event_type: EVENT_TYPE,
       }));
-      break
+      break;
     case 'result':
-      if (message.success) log('Successfully subscribed to events');
+      if (message.success) logger.info('Successfully subscribed to events');
+      break;
     case 'event':
-      rawLog(message)
-      break
+      logger.info(message);
+      break;
     default:
-      log(`Unknown message type: ${message.type}`);
-      break
+      logger.info(`Unknown message type: ${message.type}`);
+      break;
   }
-}
+};
 
 // open ws connection
-ws.on('open', function open() {
-  log('Websocket connection opened');
-})
+ws.on('open', () => {
+  logger.info('Websocket connection opened');
+});
 
 // listen for messages
-ws.on('message', function message(data) {
+ws.on('message', (data) => {
   try {
     const message = JSON.parse(data);
-    handleMessage(message)
+    handleMessage(message);
   } catch (error) {
-    error(error);
+    logger.error(error);
   }
-})
+});
 
 // listen for errors
-ws.on('error', function error(error) {
-  error(error);
-})
+ws.on('error', (error) => {
+  logger.error(error);
+});
